385dfb797d46845bd827d259d006e955a7549c98
kobakon <138418858+kobakon@users.noreply.github.com>
2023-10-13 07:42:45 +0000

    swiperの学習

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  loop: true, //最後までスライドしたら最初の画像に戻る
+  
+  //ページネーション表示の設定
+  pagination: {
+    el: '.swiper-pagination',//ページネーションの要素
+  },
+  
+  //ナビゲーションボタン（矢印）表示の設定
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
